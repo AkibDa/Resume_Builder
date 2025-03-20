209c5eee6ba68a48832a40f3008d8bd0ae2d702f
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-03-20 18:33:25 +0530

    Made a resume builder

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 from groq import Groq
-import os
+import os, datetime
 
 client = Groq(
   api_key=os.environ.get('GROQ_API_KEY'),
 )
+
+with open("resume.txt", "r") as resume_file:
+  resume = resume_file.read()
+  
+with open("job_description.txt", "r") as job_description_file:
+  jobDescrption = job_description_file.read()
+
 completion = client.chat.completions.create(
     model="llama-3.3-70b-versatile",
     messages=[
       {
         "role": "user",
-        "content": "Build a custom resume for this job posting"
+        "content": "Build a custom resume for this job posting here is the resume: "+ resume+ " and here is the job description: "+ jobDescrption
         },
       {
         
         "role": "assistant",
-        "content": "Sure, I can help you with that. Can you please provide me with the job posting?"
+        "content": "Please provide the job posting details, and I'll create a resume tailored to the job description."
         }],
     temperature=1,
     max_completion_tokens=1024,
@@ -25,3 +32,8 @@ completion = client.chat.completions.create(
 
 for chunk in completion:
     print(chunk.choices[0].delta.content or "", end="")
+    
+timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+output_file = "resume-"+timestamp+".txt"
+with open(output_file, "w") as output:
+    output.write(completion.choices[0].message.content)
